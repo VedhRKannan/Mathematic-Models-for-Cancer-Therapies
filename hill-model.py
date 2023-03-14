71b2aef9d53686ab153241ea84bcebfb73ec97f2
VedhRKannan <vedhkannan@gmail.com>
2023-03-14 13:48:03 +0000

    rdkit egfr

--- hill-model.py
+++ hill-model.py
@@ -1,26 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-# Define Hill equation function
-
-
-def hill(x, Kd, n):
-    return (x**n)/(Kd + x**n)
+from scipy.optimize import curve_fit
 
 
-# Define ligand concentration range
-ligand = np.logspace(-3, 3, 1000)
+def hill_equation(x, bmax, kd, n):
+    return bmax * (x ** n) / ((kd ** n) + (x ** n))
 
-# Define Kd and Hill coefficient
-Kd = 1.0  # dissociation constant
-n = 10  # Hill coefficient
 
-# Compute receptor activation
-receptor_activity = hill(ligand, Kd, n)
+egf_conc = np.array([0.1, 0.3, 1, 3, 10])   # EGF concentrations in nM
+# measured binding values in fmol/mg protein
+binding = np.array([2.1, 5.8, 16.3, 46.7, 88.6])
 
-# Plot results
-plt.semilogx(ligand, receptor_activity)
-plt.xlabel('Ligand concentration (nM)')
-plt.ylabel('Receptor activity')
-plt.title('EGFR activation by EGF ligand binding')
+max_binding = np.max(binding)   # maximum binding capacity
+binding_norm = binding / max_binding   # normalize to maximum binding
+binding_norm = binding_norm * 1000   # convert to fmol/μg protein
+
+p0 = [100, 1e-9, 2]   # initial guesses for the parameters
+popt, pcov = curve_fit(hill_equation, egf_conc, binding_norm, p0)
+bmax_fit, kd_fit, n_fit = popt
+
+plt.plot(egf_conc, binding_norm, 'ro', label='Experimental Data')
+plt.plot(egf_conc, hill_equation(egf_conc, bmax_fit,
+         kd_fit, n_fit), 'b-', label='Hill Equation Fit')
+plt.legend()
+plt.xlabel('EGF Concentration (nM)')
+plt.ylabel('Normalized Binding (fmol/μg protein)')
 plt.show()
